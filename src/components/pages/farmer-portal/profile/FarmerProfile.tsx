c0b9930990c4cbbbaea1cd22d060c6c66c4d9821
EpaphrasSam <isinesam@gmail.com>
2024-07-01 15:13:53 +0000

    Set up database schema for backend implementation

--- src/components/pages/farmer-portal/profile/FarmerProfile.tsx
+++ src/components/pages/farmer-portal/profile/FarmerProfile.tsx
@@ -13,25 +13,7 @@ import {
   CardBody,
 } from "@nextui-org/react";
 import { FaEdit, FaCamera } from "react-icons/fa";
-
-const regions = [
-  "Greater Accra Region",
-  "Ashanti Region",
-  "Central Region",
-  "Oti Region",
-  "Eastern Region",
-  "Brong Ahafo Region",
-  "Northern Region",
-  "Upper East Region",
-  "Upper West Region",
-  "Western Region",
-  "Savannah Region",
-  "Bono East Region",
-  "Ahafo Region",
-  "North East Region",
-  "Western North Region",
-  "Volta Region",
-];
+import { regions } from "@/lib/constants";
 
 interface FarmDetails {
   profilePicture: string | null;
--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,1 +1,28 @@
-export const categories = ["Fruits", "Onions", "Tomatoes", "Carrots"];
+export const categories = [
+  "Fruits",
+  "Vegetables",
+  "Meat",
+  "Dairy",
+  "Grains",
+  "Fish",
+  "Other",
+];
+
+export const regions = [
+  "Greater Accra Region",
+  "Ashanti Region",
+  "Central Region",
+  "Oti Region",
+  "Eastern Region",
+  "Brong Ahafo Region",
+  "Northern Region",
+  "Upper East Region",
+  "Upper West Region",
+  "Western Region",
+  "Savannah Region",
+  "Bono East Region",
+  "Ahafo Region",
+  "North East Region",
+  "Western North Region",
+  "Volta Region",
+];
