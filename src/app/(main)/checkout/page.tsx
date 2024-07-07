b6b3fc6df520fcf97c085de04cef360417c704d6
EpaphrasSam <isinesam@gmail.com>
2024-07-07 13:13:31 +0000

    Wrapped checkout in Suspense for useSearchParams

--- src/app/(main)/checkout/page.tsx
+++ src/app/(main)/checkout/page.tsx
@@ -1,5 +1,17 @@
 import Checkout from "@/components/pages/checkout/Checkout";
+import { Spinner } from "@nextui-org/react";
+import { Suspense } from "react";
 
 export default function CheckoutPage() {
-  return <Checkout />;
+  return (
+    <Suspense
+      fallback={
+        <div className="h-screen flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      }
+    >
+      <Checkout />
+    </Suspense>
+  );
 }
