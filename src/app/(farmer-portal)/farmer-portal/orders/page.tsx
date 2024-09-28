af1f8908db930d6397e3826ab28e091f0df561ab
EpaphrasSam <isinesam@gmail.com>
2024-09-28 03:48:46 +0000

    Fixed issue with farmers orders fetching

--- src/app/(farmer-portal)/farmer-portal/orders/page.tsx
+++ src/app/(farmer-portal)/farmer-portal/orders/page.tsx
@@ -7,7 +7,10 @@ export const dynamic = "force-dynamic";
 
 export default async function OrdersPage() {
   const session = await auth();
-  const { orders, error } = await getFarmerOrders(session?.user?.id!);
+  const { orders, error } = await getFarmerOrders(
+    session?.user?.farmerDetails?.id!
+  );
+
   return (
     <div className="sm:p-6 p-3">
       <div className="flex items-center gap-2 text-2xl font-bold">
